40e5b050c18c965d938c7e9147e830bec23ab447
Владислав <132227845+VladislavBroPiton@users.noreply.github.com>
2026-04-29 17:39:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else []
+# Google Calendar
+CALENDAR_ID = os.getenv("CALENDAR_ID")
